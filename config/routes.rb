2ee2db06e42346989784a0b3e523210e69fb927b
cbrenner04 <christopher.brenner1@northwestern.edu>
2016-06-11 12:16:24 -0500

    Update devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
+# frozen_string_literal: true
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
   resources :users
-  root 'users#index'
-  # The priority is based upon order of creation: first created -> highest priority.
+  root "users#index"
+  # The priority is based upon order of creation: first created ->
+  # highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
@@ -14,7 +19,8 @@ Rails.application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
+  # Example resource route (maps HTTP verbs to controller actions
+  # automatically):
   #   resources :products
 
   # Example resource route with options:
