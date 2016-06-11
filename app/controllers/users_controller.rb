24a93f35d463e2bd8f46a0ac78eea8b0d559aab1
cbrenner04 <christopher.brenner1@northwestern.edu>
2016-06-11 18:21:21 -0500

    Get tests passing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class UsersController < ApplicationController
-  before_action :authenticate_user!
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /users
   # GET /users.json
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 require "test_helper"
+require "devise"
 
 # users controller test
 class UsersControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
   setup do
     @user = users(:one)
+    sign_in users(:two)
   end
 
   test "should get index" do
@@ -21,13 +25,13 @@ class UsersControllerTest < ActionController::TestCase
   test "should create user" do
     assert_difference("User.count") do
       post :create, user: {
-        birth_date: @user.birth_date,
-        email: @user.email,
-        first_name: @user.first_name,
-        height: @user.height,
-        last_name: @user.last_name,
-        password: @user.password,
-        weight: @user.weight
+        birth_date: Time.zone.today - 20.years,
+        email: "fake@example.com",
+        first_name: "First",
+        height: 72,
+        last_name: "Last",
+        password: SecureRandom.hex(10),
+        weight: 150
       }
     end
 
@@ -46,13 +50,13 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should update user" do
     patch :update, id: @user, user: {
-      birth_date: @user.birth_date,
-      email: @user.email,
-      first_name: @user.first_name,
-      height: @user.height,
-      last_name: @user.last_name,
-      password: @user.password,
-      weight: @user.weight
+      birth_date: Time.zone.today - 21.years,
+      email: "whatever@example.com",
+      first_name: "First",
+      height: 60,
+      last_name: "Last",
+      password: SecureRandom.hex(10),
+      weight: 160
     }
     assert_redirected_to user_path(assigns(:user))
   end
