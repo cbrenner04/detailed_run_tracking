1a9eafbd342e231c1075ace9a0a25fe2f83419d7
cbrenner04 <christopher.brenner1@northwestern.edu>
2016-06-17 14:45:41 -0500

    CLean up

--- app/helpers/runs_helper.rb
+++ app/helpers/runs_helper.rb
@@ -1,3 +1,55 @@
 # frozen_string_literal: true
 module RunsHelper
+  def distance_for(run)
+    unless run.sum(:distance) == 0
+      distance = run.sum(:distance).round(2)
+      "#{distance} miles"
+    end
+  end
+
+  def total_duration(run)
+    format_time(run.duration)
+  end
+
+  def pace(duration:, distance:)
+    unless distance == 0
+      duration_per_mile = (duration / distance)
+      "#{format_time(duration_per_mile)} min/mile"
+    end
+  end
+
+  def month_runs
+    @runs.where(
+      occurred_at: Time.zone.now.beginning_of_month..Time.zone.now.end_of_month
+    )
+  end
+
+  def week_runs
+    @runs.where(
+      occurred_at: Time.zone.now.beginning_of_week..Time.zone.now.end_of_week
+    )
+  end
+
+  def hours(time)
+    time.nil? ? 0 : time / (60 * 60)
+  end
+
+  def minutes(time)
+    time.nil? ? 0 : (time / 60) % 60
+  end
+
+  def seconds(time)
+    time.nil? ? 0 : time % 60
+  end
+
+  private
+
+  def format_time(duration)
+    if hours(duration) < 1
+      format("%02d:%02d", minutes(duration), seconds(duration))
+    else
+      format("%02d:%02d:%02d", hours(duration), minutes(duration),
+             seconds(duration))
+    end
+  end
 end
